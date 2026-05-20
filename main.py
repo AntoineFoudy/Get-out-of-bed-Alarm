e693b52644b5b242a3241ba312cf3f549ed8fceb
AntoineFoudy <145511347+AntoineFoudy@users.noreply.github.com>
2026-05-20 22:49:20 +0100

    Schedule Alarm and End point to stop the alarm
    - schedule_alarm runs in its own separate thread and counts to wake_up_time before starting the alarm
    
    - Use Flask to sent up a REST API to call the stop_alarm method and only stops the alarm if the request is from a certain IP address

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
 import time
+from datetime import datetime, timedelta
+import threading
+from flask import Flask, request
 import alarm
 
-a = alarm.Alarm()
-a.start_alarm()
+app = Flask(__name__)
 
-time.sleep(60)
-a.stop_alarm()
+alarm_instance = alarm.Alarm() # Create an instance of the Alarm class
+client_IP_address = "192.168.0.x" # Stop the alarm from being shut off from different IP addresses
+
+def schedule_alarm():
+    while True:
+        now = datetime.now()
+        wake_up_time = now.replace(hour=5, minute=0, second=0, microsecond=0)
+
+        if now >= wake_up_time:
+            wake_up_time += timedelta(days=1)
+
+        time_until_alarm = (wake_up_time - now).total_seconds()
+
+        time.sleep(time_until_alarm)
+        alarm_instance.start_alarm()
+
+threading.Thread(target=schedule_alarm, daemon=True).start() # Start the alarm scheduling in a separate thread
+
+
+@app.route('/stop_alarm', methods=['POST']) # Define a route to stop the alarm
+def stop_alarm():
+    if request.remote_addr == client_IP_address:
+        alarm_instance.stop_alarm()
+        return "", 204
+    return "", 403
+
+app.run(host="0.0.0.0", port=5000)
+
+
+
